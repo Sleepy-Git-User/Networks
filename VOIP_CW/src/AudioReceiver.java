f904e49ed5b3e32cc7c916520ad13c6941a6a008
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2023-03-10 20:23:57 +0000

    Update AudioReceiver.java

--- VOIP_CW/src/AudioReceiver.java
+++ VOIP_CW/src/AudioReceiver.java
@@ -62,11 +62,11 @@ public class AudioReceiver implements Runnable {
 
         boolean running = true;
         sequenceLayer sl = new sequenceLayer();
-        int count = 0;
-        byte[][] send = new byte[16][];
-        byte[][] history = new byte[16][];
-        HashSet<Integer> set = new HashSet<Integer>();
-        HashMap<Integer, byte[]> hashmap = new HashMap<Integer, byte[]>();
+        int count = 0; //Count to keep track of the number of packets in the array
+        byte[][] send = new byte[16][]; //Array play packets
+        byte[][] history = new byte[16][]; //Array to store the played packets
+        HashSet<Integer> set = new HashSet<Integer>(); //Set to store the headers to check for duplicates
+        HashMap<Integer, byte[]> hashmap = new HashMap<Integer, byte[]>(); //Hashmap to store the packets to be played later
         while (running){
 
             try{
@@ -79,45 +79,52 @@ public class AudioReceiver implements Runnable {
 
                 receiving_socket.receive(packet);
 
-
+                //Gets header
                 short header = sl.getHeader(buffer);
 
-
+                /*
+                If the header is 3 it signifies the start of a new packet
+                and if the count is more than 15 it signifies that the array is full
+                Both cause the array to be played
+                 */
                 if(count<15 & header != 3){
                     //System.out.println("Receiver " + (int) header);
-                    if(set.contains((int) header)){
+                    if(set.contains((int) header)){ //Adds to hashmap if the header is already in the set
 //                        System.out.println("Packet Lost");
-                        hashmap.put((int) header, buffer);
-
+                        hashmap.put((int) header, buffer); //Stores in hashmap to be played later
                         count++;
                         continue;
                     }
 
-                    send[header] = buffer;
-                    set.add((int) header);
+                    send[header] = buffer; //Adds to the array to be played
+                    set.add((int) header); //Adds to the set
                     count++;
                 }
 
                 else{
-                   for(int i=0; i<16; i++){
-                        count = 0;
+                    for(int i=0; i<16; i++){ //Plays all the packets in the array
+                        count = 0; //Resets the count
 //                       System.out.println("Receiver " +  Arrays.toString(send[i]));
                         history[i] = send[i];
-                       if (send[i] != null) {
-                           if (sl.getHeader(send[i]) == (short) i) {
-                               ap.playBlock(sl.getAudio(send[i]));
-                               send[i] = hashmap.getOrDefault(i, null);
-                               if(hashmap.remove(i) != null){
-                                   count++;
-                               }
+                        //Stores the packets in the history array
+                       //Which can be used for compensation
+                        if (send[i] != null) {
+                           //Play packet
+                            if (sl.getHeader(send[i]) == (short) i) {
+                                ap.playBlock(sl.getAudio(send[i]));
+                               //Checks if the packet is in the hashmap if it is add it to the array
+                                send[i] = hashmap.getOrDefault(i, null); //If not add null to the array
+                                if(hashmap.remove(i) != null){ //If the packet is in the hashmap remove it
+                                    count++; //Increment the count
+                                }
 //                               send[i] = hashmap.get(i);
-                           }
-                       }
+                            }
+                        }
                     }
-                    set.clear();
-                    set.add((int) header);
-                    send[header] = buffer;
-                    count++;
+                    set.clear(); //Clears the set
+                    set.add((int) header); //Adds the new header to the set
+                    send[header] = buffer; //Adds the new packet to the array
+                    count++; //Increments the count
                 }
 
 
