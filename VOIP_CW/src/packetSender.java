a1c1703b6505a064622f471796d51df894e4a9fa
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2023-03-16 12:26:26 +0000

    Update packetSender.java

--- VOIP_CW/src/packetSender.java
+++ VOIP_CW/src/packetSender.java
@@ -1,3 +1,5 @@
+import jdk.incubator.foreign.SequenceLayout;
+
 import java.net.*;
 import java.io.*;
 import java.nio.ByteBuffer;
@@ -6,23 +8,30 @@ public class packetSender {
     static DatagramSocket sending_socket;
     protected InetAddress clientIP;
     protected int PORT;
+    protected sequenceLayer sl;
 
     public packetSender(InetAddress clientIP, int PORT) throws IOException {
         this.clientIP = clientIP;
         this.PORT = PORT;
+        sl = new sequenceLayer();
+        sending_socket = new DatagramSocket();
     }
 
-    public void send(byte[] data) throws IOException {
+    public void send(ByteBuffer data) throws IOException {
+
         //DatagramSocket sending_socket;
         try{
-            DatagramSocket sending_socket = new DatagramSocket();
+            if(data.hasArray()){
+
+                DatagramPacket packet = new DatagramPacket(data.array(), data.array().length, clientIP, PORT);
+
+                sending_socket.send(packet);
+                //***************************************************
+                //Close the socket
+                sending_socket.close();
+            }
             //***************************************************
             //Send the data
-            DatagramPacket packet = new DatagramPacket(data, data.length, clientIP, PORT);
-            sending_socket.send(packet);
-            //***************************************************
-            //Close the socket
-            sending_socket.close();
 
         } catch (SocketException e){
             System.out.println("ERROR: TextSender: Could not open UDP socket to send from.");
