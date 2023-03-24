fb7e3508482de7e64f8d2ec1409baabe7a2a0d33
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2023-03-24 16:26:59 +0000

    Revert "Revert "Hand Shake sorted""
    This reverts commit c0ee202eeab74d215b44e560d7afe096fcff5409.

--- VOIP_CW/src/rsaReceiver.java
+++ VOIP_CW/src/rsaReceiver.java
@@ -146,6 +146,7 @@ public class rsaReceiver implements Runnable{
                     System.out.println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
                     rsaSender.acknowledgement = true;
                     rsaSender.haveXor = true;
+                    rsaSender.finished = true;
                     running = false;
                     try {
                         Thread.sleep(7000); // sleep for 1 second
